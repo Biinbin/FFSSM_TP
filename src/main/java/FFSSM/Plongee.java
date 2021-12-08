ef52b33778b43d2d918769d9fd78244ee77213a3
Biinbin <gabin.lasservarie@etud.univ-jfc.fr>
2021-12-08 10:34:22 +0100

    FinTPFFSSM

--- src/main/java/FFSSM/Plongee.java
+++ src/main/java/FFSSM/Plongee.java
@@ -4,47 +4,56 @@
 package FFSSM;
 
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
 
 public class Plongee {
 
-	public Site lieu;
+    public Site lieu;
 
-	public Moniteur chefDePalanquee;
+    public Moniteur chefDePalanquee;
 
-	public LocalDate date;
+    public LocalDate date;
 
-	public int profondeur;
+    public int profondeur;
 
-	public int duree;
+    public int duree;
 
-	public Plongee(Site lieu, Moniteur chefDePalanquee, LocalDate date, int profondeur, int duree) {
-		this.lieu = lieu;
-		this.chefDePalanquee = chefDePalanquee;
-		this.date = date;
-		this.profondeur = profondeur;
-		this.duree = duree;
-	}
+    public ArrayList<Plongeur> plongeurList;
 
-	public void ajouteParticipant(Plongeur participant) {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
-	}
+    public Plongee(Site lieu, Moniteur chefDePalanquee, LocalDate date, int profondeur, int duree) {
+        this.lieu = lieu;
+        this.chefDePalanquee = chefDePalanquee;
+        this.date = date;
+        this.profondeur = profondeur;
+        this.duree = duree;
+        this.plongeurList = new ArrayList<>();
+    }
 
-	public LocalDate getDate() {
-		return date;
-	}
+    public Plongee(Moniteur chefDePalanquee) {
+        this.chefDePalanquee = chefDePalanquee;
+    }
+   
+    public void ajouteParticipant(Plongeur participant) {
+        plongeurList.add(participant);
+    }
 
-	/**
-	 * Détermine si la plongée est conforme. 
-	 * Une plongée est conforme si tous les plongeurs de la palanquée ont une
-	 * licence valide à la date de la plongée
-	 * @return vrai si la plongée est conforme
-	 */
-	public boolean estConforme() {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
-	}
+    public LocalDate getDate() {
+        return date;
+    }
 
+    /**
+     * Détermine si la plongée est conforme. Une plongée est conforme si tous
+     * les plongeurs de la palanquée ont une licence valide à la date de la
+     * plongée
+     *
+     * @return vrai si la plongée est conforme
+     */
+    public boolean estConforme() {
+        for (Plongeur p : plongeurList) {
+            if (p.derniereLicence().estValide(date) == true) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
